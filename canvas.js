f40641234308e7e6b6a5d40011bf4a9e28bba58f
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2023-02-07 13:30:36 -0500

    fix misnamed variable

--- canvas.js
+++ canvas.js
@@ -15,8 +15,8 @@ addEventListener("resize", (event) => {
 });
 
 $(document).ready(function(){
-  let c = new circleCharge(0,c.width/2,c.height/2,5);
-  charges.push(c)
+  let circ = new circleCharge(0,c.width/2,c.height/2,5);
+  charges.push(circ)
 });
 
 function renderObjs() {
