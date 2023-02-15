d0e50791e48ae5b68ee06ae4b0615dba16456904
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2023-02-15 13:50:46 -0500

    change fakemax radius

--- canvas.js
+++ canvas.js
@@ -45,7 +45,7 @@ function shadeField(){
   for(let i = 0; i < charges.length; i++){
     totalCharge += charges[i].charge;
   }
-  let fakemax = (totalCharge/(4*Math.PI*e_0*(0.1**2)));
+  let fakemax = (totalCharge/(4*Math.PI*e_0*(9**2)));
   for(let x = 0; x < canvas.width; x++){
     for(let y = 0; y < canvas.height; y++){
       let fieldAtPoint = 0;
