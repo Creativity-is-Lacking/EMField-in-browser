40922bcb3c9e98781b8614922d1058cb12221537
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2023-02-15 14:08:37 -0500

    redo hue calculation

--- canvas.js
+++ canvas.js
@@ -53,7 +53,7 @@ function shadeField(){
         let r = Math.sqrt((x-charges[i].x)**2 + (y-charges[i].y)**2);
         fieldAtPoint += (charges[i].charge/(4*Math.PI*e_0*(r**2)));
       }
-      var hue = (fieldAtPoint/(fakemin))%320
+      var hue = (fakemin/-fieldAtPoint)%320
       c.fillStyle = "hsl("+hue+",100%, 50%)";
       c.fillRect(x, y, 1, 1 );
     }
